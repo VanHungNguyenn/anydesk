7a368c086f4c3748f02632121c04b2bf0256e3dc
VanHungNguyen <vanhungnguyenbku@gmail.com>
2022-05-05 10:33:45 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import { languages } from './constants'
 import Cookies from 'js-cookie'
 import axios from 'axios'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+	const { i18n } = useTranslation()
+
 	const [currentLang, setCurrentLang] = useState(Cookies.get('i18next'))
 
-	const getData = async () => {
+	const getData = useCallback(async () => {
 		const res = await axios.get('https://geolocation-db.com/json/')
 
 		setCurrentLang(
 			languages.find(
 				(lang) =>
 					lang.country_code === res.data.country_code.toLowerCase()
-			)?.code
+			)?.code || 'en'
 		)
-	}
+		console.log(currentLang)
+		i18n.changeLanguage(currentLang)
+	}, [currentLang, i18n])
 
 	useEffect(() => {
-		getData()
-	}, [])
+		if (!currentLang) {
+			getData()
+			// reload page
+			window.location.reload()
+		}
+	}, [currentLang, getData])
 
 	return (
 		<Routes>
